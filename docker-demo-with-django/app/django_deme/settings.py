3a1ef29bca8b96cc85d9b72b1a44ded3d642b90f
NaokiMitamura <snbyqsnbyq@gmail.com>
2021-10-17 15:52:55 +0900

    hello

--- docker-demo-with-django/app/django_deme/settings.py
+++ docker-demo-with-django/app/django_deme/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #追加
+    'helloworldapp',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'django_deme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
